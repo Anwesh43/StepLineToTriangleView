b2019a980fa23b265904ddd375997cd4df8757cd
Anwesh43 <anweshthecool0@gmail.com>
2020-01-09 10:44:15 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtricreaterotbouncyview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtricreaterotbouncyview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtricreaterotbouncyview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.tricreaterotbouncyview.TriCreateRotBouncyView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriCreateRotBouncyView.create(this)
     }
 }
