02d5d63d067fa93f3232f380e3fd9b27f98eea38
Anwesh43 <anweshthecool0@gmail.com>
2020-01-09 10:06:02 +0530

    adding constants and libraries

--- tricreaterotbouncyview/src/main/java/com/anwesh/uiprojects/tricreaterotbouncyview/TriCreateRotBouncyView.kt
+++ tricreaterotbouncyview/src/main/java/com/anwesh/uiprojects/tricreaterotbouncyview/TriCreateRotBouncyView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.tricreaterotbouncyview
 /**
  * Created by anweshmishra on 09/01/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 6
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val deg : Float = 60f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
