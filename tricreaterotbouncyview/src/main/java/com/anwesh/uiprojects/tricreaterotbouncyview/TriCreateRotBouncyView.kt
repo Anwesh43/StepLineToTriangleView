f8c36e2741d4548880042b0629d1f79b7ce8a1d8
Anwesh43 <anweshthecool0@gmail.com>
2020-01-09 10:29:18 +0530

    adding TCRBNode class

--- tricreaterotbouncyview/src/main/java/com/anwesh/uiprojects/tricreaterotbouncyview/TriCreateRotBouncyView.kt
+++ tricreaterotbouncyview/src/main/java/com/anwesh/uiprojects/tricreaterotbouncyview/TriCreateRotBouncyView.kt
@@ -117,4 +117,46 @@ class TriCreateRotBouncyView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TCRBNode(var i : Int, val state : State = State()) {
+
+        private var next : TCRBNode? = null
+        private var prev : TCRBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TCRBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTCRBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TCRBNode {
+            var curr : TCRBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
